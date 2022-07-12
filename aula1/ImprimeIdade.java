dd6a37baf70194193e3e5815c901edb9ae0a3db3
Ariane <amprvieira@gmail.com>
2022-07-12 12:21:00 -0300

    Função IMC

---
+++ aula1/ImprimeIdade.java
@@ -0,0 +1,28 @@
+package aula1;
+
+public class ImprimeIdade {
+    public static void main(String[] args) {
+        int idade = 40;
+        String nome = "Victor Icoma";
+        double altura = 1.88;
+        double peso = 132.56;
+
+        double imc = peso / (altura * altura);
+
+        System.out.println("Ola: " + nome);
+        if (idade >= 40) {
+            if (imc < 22) {
+                System.out.println("IMC: " + imc + "sua idade e " + idade + " risco de subnutricao.");
+            } else if (imc <= 30) {
+                System.out.println("IMC: " + imc + "sua idade e " + idade + " isso ai garota(o)");
+            } else if (imc <= 35) {
+                System.out.println("IMC: " + imc + "sua idade e " + idade + " Opa! Você pode estar pesado. Cuidade!");
+            } else {
+                System.out.println("IMC: " + imc + "sua idade e " + idade + " procure um médico e um nutricionista.");
+            }
+
+        } else {
+            System.out.println("Novo demais para medicacao");
+        }
+    }
+}
